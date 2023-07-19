fc71e6f2bf4e2b477946d2253d41205921eff7be
aihassan1 <abdosruor57@gmail.com>
2023-07-19 14:33:14 +0000

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,21 +13,20 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 result = i * j;
+
 if (j != 0)
 {
 _putchar(',');
 _putchar(' ');
 }
-
 if (result < 10 && j != 0)
 {
 _putchar(' ');
 }
-else
+else if (result >= 10)
 {
 _putchar(result / 10 + '0');
 }
-
 _putchar(result % 10 + '0');
 }
 _putchar('\n');

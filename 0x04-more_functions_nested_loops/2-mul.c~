a99b66a4f1d398b8263b324047dbb5178ac56429
aihassan1 <abdosruor57@gmail.com>
2023-07-20 17:13:49 +0000

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c~
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int mul(int a, int b)
+{
+  int result;
+  result = a * b;
+  _putchar('0' + result);
+  return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a new line.
+ *
+ * Description:
+ * This function prints the numbers from 0 to 9 on separate lines using the
+ * _putchar function. It iterates through a loop, where the loop variable 'i'
+ * takes values from 0 to 9, and each number is printed followed by a newline.
+ *
+ * Return: void (no return value)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+_putchar('\n');
+}
+}

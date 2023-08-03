d59115d52e922c1a1e363ddf949f2e69c2a227a5
aihassan1 <abdosruor57@gmail.com>
2023-08-03 01:23:40 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,8 +12,8 @@ int is_prime_number(int n)
 if (n <= 1)
 {
 return (0);
-return (actual_prime(n, n - 1));
 }
+return (actual_prime(n, n - 1));
 }
 /**
  * actual_prime - Calculates if a number is prime recursively
@@ -29,6 +29,6 @@ return (1);
 if ((n % i) == 0 && i > 0)
 {
 return (0);
-return (actual_prime(n, i - 1));
 }
+return (actual_prime(n, i - 1));
 }

ecf84ad8d2c4cd81ea77a136a979b5cdc4e75cff
aihassan1 <abdosruor57@gmail.com>
2023-08-16 01:13:32 +0000

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ if (d == NULL)
 return;
 
 if (d->name == NULL)
-printf("Name: (nil)");
+printf("Name: (nil)\n");
 
 if (d->owner == NULL)
-printf("Owner: (nil)");
+printf("Owner: (nil)\n");
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }

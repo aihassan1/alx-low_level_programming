17dab7ea9584856d99c59f4ccce6ac659733a13d
aihassan1 <abdosruor57@gmail.com>
2023-08-25 02:56:10 +0000

    addnodeedn

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+* add_node_end - Adds a new node at the END of a linked list.
+* @head: Double pointer to the head of the linked list.
+* @str: String to be stored in the new node.
+* Return: Address of the new head of the linked list.
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *ptr = *head;
+list_t *new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+
+new_node->str = strdup(str);
+
+if (new_node->str == NULL)
+{
+return (NULL);
+}
+
+
+if (*head == NULL)
+{
+*head = new_node;
+}
+else
+{
+while (ptr->next != NULL)
+{
+ptr = ptr->next;
+}
+ptr->next = new_node;
+}
+
+new_node->next = NULL;
+return (*head);
+
+}

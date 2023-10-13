7b2e8496a4d3dbeac8f1e551936457bc8362ba89
aihassan1 <abdosruor57@gmail.com>
2023-10-13 15:53:45 +0000

    7-insert_dnodeint.ca

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,20 +1,41 @@
 #include "lists.h"
-
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 /**
- * insert_dnodeint_at_index - Inserts a new node at a specific index.
- * @h: A pointer to a pointer to the head of the doubly linked list.
- * @idx: The index at which to insert the new node.
- * @n: The value to store in the new node.
- *
- * Return: A pointer to the new node, or NULL if it fails.
- */
+* insert_dnodeint_at_index - Inserts a new node at a specific index.
+* @h: A pointer to a pointer to the head of the doubly linked list.
+* @idx: The index at which to insert the new node.
+* @n: The value to store in the new node.
+*
+* Return: A pointer to the new node, or NULL if it fails.
+*/
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *temp;
 dlistint_t *node;
 dlistint_t *new_node;
+dlistint_t *add_dn;
+
+if (*h == NULL)
+{
+node = get_dnodeint_at_index(*h, (idx - 1));
+if (node == NULL)
+	return (NULL);
+
+node->next = NULL;
+node->prev = NULL;
+node->n = n;
+return (node);
+}
+
+if (idx == 0)
+{
+add_dn = add_dnodeint(h, n);
+if (add_dn == NULL)
+	return (NULL);
+else
+	return (add_dn);
+}
 
 node = get_dnodeint_at_index(*h, (idx - 1));
 if (node == NULL)
@@ -28,7 +49,6 @@ free(*h);
 free_dlistint(node);
 return (NULL);
 }
-
 
 /*fill the node info*/
 new_node->n = n;
@@ -46,12 +66,12 @@ return (new_node);
 }
 
 /**
- * get_dnodeint_at_index - Retrieves a node at a specific index.
- * @head: A pointer to the head of the doubly linked list.
- * @index: The index of the node to retrieve.
- *
- * Return: A pointer to the node at the specified index, or NULL if not found.
- */
+* get_dnodeint_at_index - Retrieves a node at a specific index.
+* @head: A pointer to the head of the doubly linked list.
+* @index: The index of the node to retrieve.
+*
+* Return: A pointer to the node at the specified index, or NULL if not found.
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 dlistint_t *temp = NULL;
@@ -65,9 +85,9 @@ temp = head;
 while (temp != NULL)
 {
 if (counter == index)
-return (temp);
+	return (temp);
 else if (temp == NULL)
-return (NULL);
+	return (NULL);
 
 temp = temp->next;
 counter++;

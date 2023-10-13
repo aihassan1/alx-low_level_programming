4030a82829310383d6c3d6375307fd9cf6216d9a
aihassan1 <abdosruor57@gmail.com>
2023-10-13 08:11:54 +0000

    222

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 
 dlistint_t *new_node = malloc(sizeof(dlistint_t));
- if (new_node == NULL)
+if (new_node == NULL)
 {
 dprintf(2, "Error: Can't malloc\n");
+exit(1)
 }
 
 new_node->n = n;

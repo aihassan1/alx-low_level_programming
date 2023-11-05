bb038029cb99e0226036ecb62be287a9441ebf6c
aihassan1 <abdosruor57@gmail.com>
2023-11-05 17:25:19 +0000

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,28 +9,28 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *temp_node_ptr = NULL;
-unsigned long int index;
+    hash_node_t *temp_node_ptr = NULL;
+    unsigned long int index;
 
-if (ht == NULL || key == NULL)
-return (NULL);
+    if (ht == NULL || key == NULL)
+        return (NULL);
 
-index = key_index((const unsigned char *)key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
 
-temp_node_ptr = ht->array[index];
+    temp_node_ptr = ht->array[index];
 
-if (temp_node_ptr == NULL)
-{
-return (NULL);
-}
+    if (temp_node_ptr == NULL)
+    {
+        return (NULL);
+    }
 
-while (temp_node_ptr != NULL) 
-{
-if (strcmp(key, temp_node_ptr->key) == 0)
-{
-return (temp_node_ptr->value);
-}
-temp_node_ptr = temp_node_ptr->next;
-}
-return (NULL);
+    while (temp_node_ptr != NULL)
+    {
+        if (strcmp(key, temp_node_ptr->key) == 0)
+        {
+            return (temp_node_ptr->value);
+        }
+        temp_node_ptr = temp_node_ptr->next;
+    }
+    return (NULL);
 }

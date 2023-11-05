416633046aefd07f2c44e3b8c4e27a4d774d0bcb
aihassan1 <abdosruor57@gmail.com>
2023-11-05 02:33:32 +0000

    2-

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 unsigned long int hashed_value;
 	
 if (key == NULL)
-return (0);
+return (-1);
 
 hashed_value = hash_djb2(key) % size;
 return (hashed_value);

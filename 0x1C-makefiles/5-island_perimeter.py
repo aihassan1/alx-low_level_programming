d714b9fcfb29275d1b2848a1f61f54b992c64a51
aihassan1 <abdosruor57@gmail.com>
2023-12-22 02:30:16 +0200

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+""" this is module comment """
 def island_perimeter(grid):
+    """this is a def for the function """
     counter = 0
     for small_list in grid:
         for item in small_list:

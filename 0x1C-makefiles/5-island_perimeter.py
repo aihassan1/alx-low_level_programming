ea1c17fa2454c66fca9c08dd39026e830e00ee8b
aihassan1 <abdosruor57@gmail.com>
2023-12-23 18:01:44 +0200

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ this is module comment """
 
+
 def island_perimeter(grid):
     """this is a def for the function"""
     counter = 0
@@ -8,22 +9,22 @@ def island_perimeter(grid):
     grid_width = len(grid[0]) - 1
 
     for index, sublist in enumerate(grid):
-        for list_index, element in enumerate(sublist):
+        for l_index, element in enumerate(sublist):
             if element == 1:
                 # top ele
-                if index == 0 or grid[index - 1][list_index] != 1:
+                if index == 0 or grid[index - 1][l_index] != 1:
                     counter += 1
 
                 # bottom ele
-                if index == grid_length or grid[index + 1][list_index] != 1:
+                if index == grid_length or grid[index + 1][l_index] != 1:
                     counter += 1
 
                 # left ele
-                if list_index == 0 or grid[index][list_index - 1] != 1:
+                if l_index == 0 or grid[index][l_index - 1] != 1:
                     counter += 1
 
                 # right ele
-                if list_index == grid_width or grid[index][list_index + 1] != 1:
+                if l_index == grid_width or grid[index][l_index + 1] != 1:
                     counter += 1
 
     return counter
